1787d924be6b4a19258aeccab2e3c89c6dad5935
Arindam <arindamkeswani@gmail.com>
2021-11-02 12:17:51 +0530

    MondoDB Intro, createUser

--- Backend/Practice/Express/Food App/methods.js
+++ Backend/Practice/Express/Food App/methods.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 
-//Middleware function used in POST method to convert front-end data to JSON format
+ //Middleware function used in POST method to convert front-end data to JSON format
 app.use(express.json()); //3
 
 app.listen(3000);
@@ -57,6 +58,11 @@ function middleware2(req, res, next){
 
 function getUser(req,res){
     res.send(users);
+
+    //CRUD video
+    // let allUsers = await userModel.find();
+    // res.json({message:"List of all users:", 
+    //         data: allUsers});
 }
 
 function postUser(req,res){
@@ -128,3 +134,57 @@ function postSignUp(req,res){
         data:obj
     });
 }
+
+
+//MONGODB and MONGOOSE
+
+//promise based function
+
+db_link='mongodb+srv://admin:foodappadmin@cluster0.t8urz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+
+mongoose.connect(db_link)
+.then(function(db){
+    console.log("DB Connected");
+})
+.catch(function(err){
+    console.log(err);
+});
+
+const userSchema = mongoose.Schema({
+    name:{
+        type:String, 
+        required: true
+    },
+    email: {
+        type:String,
+        required:true,
+        unique: true
+    },
+    password:{
+        type:String,
+        required:true,
+        minLength:8
+    },
+    confirmPassword:{
+        type:String,
+        required:true,
+        minLength:8
+    } 
+});
+
+//model
+
+//model name, shcema name
+const userModel = mongoose.model('userModel', userSchema);
+
+(async function createUser(){
+    let user={
+        name:'Arindam 2',
+        email:"aaaa@gmail.com",
+        password: "12345678",
+        confirmPassword:"12345678"
+    }
+
+    let data = await userModel.create(user);
+    console.log(data);
+})()
---
+++ Backend/Practice/Express/Food App/secrets.js
@@ -0,0 +1,2 @@
+//foodappadmin
+//mongodb+srv://admin:<password>@cluster0.t8urz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
