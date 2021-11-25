13597b589b5b716f268eb05a43a81bd1f0b5e124
Arindam <arindamkeswani@gmail.com>
2021-11-25 12:09:36 +0530

    8.3 : Paste

--- frontend/Excel/Practice/cutCopyPaste.js
+++ frontend/Excel/Practice/cutCopyPaste.js
@@ -24,14 +24,16 @@ for (let i = 0; i < rows; i++) {
 }
 
 let rangeStorage = [];
+let rowDiff = 0;
+let colDiff = 0;
 function handleSelectedCells(cell) {
     cell.addEventListener("click", (e) => {
         //Select range of cells
         //will need a storage to keep track of selected cells
         if (!ctrlKey) {
 
-           
-            for (let i = rangeStorage.length-1; i >=0; i--) {
+
+            for (let i = rangeStorage.length - 1; i >= 0; i--) {
                 let prevCell = document.querySelector(`.cells[rid = "${rangeStorage[i][0]}"][cid = "${rangeStorage[i][1]}"]`);
                 prevCell.style.border = "1px solid #dfe4ea"
 
@@ -54,10 +56,13 @@ function handleSelectedCells(cell) {
             cell.style.border = "3px solid #218c74"
             let rid = Number(cell.getAttribute("rid"));
             let cid = Number(cell.getAttribute("cid"));
-            rangeStorage[1][0]=rid;
-            rangeStorage[1][1]=cid;
+            rangeStorage[1][0] = rid;
+            rangeStorage[1][1] = cid;
 
             console.log(rangeStorage[0], rangeStorage[1]);
+            rowDiff = Math.abs(rangeStorage[0][0] - rangeStorage[1][0])
+            colDiff = Math.abs(rangeStorage[0][1] - rangeStorage[1][1])
+
             return;
 
 
@@ -72,31 +77,80 @@ function handleSelectedCells(cell) {
         rangeStorage.push([rid, cid]);
         console.log(rangeStorage);
 
+        if (rangeStorage.length == 2) {
+            rowDiff = Math.abs(rangeStorage[0][0] - rangeStorage[1][0])
+            colDiff = Math.abs(rangeStorage[0][1] - rangeStorage[1][1])
+        }
+
     })
 }
 
 //to store copied data
 let clipboard = []
-copyBtn.addEventListener("click", (e)=>{
+copyBtn.addEventListener("click", (e) => {
+    clipboard=[];
     let srow = rangeStorage[0][0];
     let erow = rangeStorage[1][0];
     let scol = rangeStorage[0][1];
     let ecol = rangeStorage[1][1];
 
-    for(let i=Math.min(srow,erow); i<=Math.max(srow,erow); i++){
+    for (let i = Math.min(srow, erow); i <= Math.max(srow, erow); i++) {
         let copyRow = []
-        for(let j=Math.min(scol, ecol); j<=Math.max(scol, ecol); j++){
+        for (let j = Math.min(scol, ecol); j <= Math.max(scol, ecol); j++) {
             let cellProp = sheetDB[i][j];
             copyRow.push(cellProp)
         }
         clipboard.push(copyRow);
     }
 
-    for (let i = rangeStorage.length-1; i >=0; i--) {
+    for (let i = rangeStorage.length - 1; i >= 0; i--) {
         let prevCell = document.querySelector(`.cells[rid = "${rangeStorage[i][0]}"][cid = "${rangeStorage[i][1]}"]`);
         prevCell.style.border = "1px solid #dfe4ea";
     }
 
     console.log(clipboard);
 
+})
+
+
+pasteBtn.addEventListener("click", (e) => {
+    console.log("Paste clicked");
+    // if (rangeStorage.length != 2) return;
+
+    // let rowDiff = Math.abs(rangeStorage[0][0] - rangeStorage[1][0])
+    // let colDiff = Math.abs(rangeStorage[0][1] - rangeStorage[1][1])
+
+    let address = addressbar.value;
+    let [tarStartRow, tarStartCol] = decoderidcid(address)
+    console.log(tarStartRow, tarStartCol);
+    //clipRow -> refer copy row
+    //clipCol -> refer copy column
+    for (let i = tarStartRow, clipRow = 0; i <= tarStartRow + rowDiff; i++, clipRow++) {
+        for (let j = tarStartCol, clipCol = 0; j <= tarStartCol + colDiff; j++, clipCol++) {
+            let cell = document.querySelector(`.cells[rid = "${i}"][cid = "${j}"]`);
+            if (!cell) {
+                alert("Going out of spreadsheet's range.")
+                continue;
+            }
+
+            let copiedCellProp = clipboard[clipRow][clipCol]
+
+            let cellProp = sheetDB[i][j];
+
+            //Update in DB
+            cellProp.bold = copiedCellProp.bold;
+            cellProp.italic = copiedCellProp.italic;
+            cellProp.underline = copiedCellProp.underline;
+            cellProp.alignment = copiedCellProp.alignment;
+            cellProp.fontColor = copiedCellProp.fontColor;
+            cellProp.BGcolor = copiedCellProp.BGcolor;
+            cellProp.fontFamily = copiedCellProp.fontFamily;
+            cellProp.fontSize = copiedCellProp.fontSize;
+            cellProp.value = copiedCellProp.value;
+
+            //UI
+            cell.click();
+
+        }
+    }
 })
