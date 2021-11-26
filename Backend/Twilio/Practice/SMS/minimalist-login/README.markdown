04b2521615701f829479a17bed798cb2a26c9a06
Arindam <arindamkeswani@gmail.com>
2021-11-26 18:04:39 +0530

    Send OTP MK 1

---
+++ Backend/Twilio/Practice/SMS/minimalist-login/README.markdown
@@ -0,0 +1,5 @@
+# Minimalist Login 
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/felix-godinez/pen/GRvLppp](https://codepen.io/felix-godinez/pen/GRvLppp).
+
+Just, a little login for who`is that only want a template to get inspiration , for students who are searching a simple template for change some details.
--- Backend/Twilio/Practice/sms.js
+++ Backend/Twilio/Practice/sms.js
@@ -5,5 +5,5 @@ const authToken = process.env.AUTH_TOKEN;
 const client = require('twilio')(accountSid, authToken);
 
 client.messages
-      .create({body: 'Test message from Arindam using env', from: '+12569260850', to: '+918604636833'})
+      .create({body: 'Test message from Arindam', from: '+12569260850', to: '+919999508409'})
       .then(message => console.log(message.sid));
